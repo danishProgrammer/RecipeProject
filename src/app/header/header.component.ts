80daef3631d3a9f29dfbbcb71373adc14f9a21a4
Danish Arora <Danish@rebuscode.com>
2018-06-14 16:33:46 +0530

    Header Setup

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  headerHeading = 'Recipe';
+  headerList = ['Recipe','ShoppingList'];  
   constructor() { }
 
   ngOnInit() {
