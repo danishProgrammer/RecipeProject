94ca403c632ff0a094a84e3a9067dea2530ea773
Danish Arora <Danish@rebuscode.com>
2018-06-18 20:03:00 +0530

    NavigationPath using eventBinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '';
+  elementType : string;
+
+ navigatePath (selectedFeature : string){
+   if(selectedFeature == 'Recipe')
+    this.elementType = selectedFeature;
+    else
+    this.elementType = selectedFeature;
+ }
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   headerHeading = 'Recipe';
-  headerList = ['Recipe','ShoppingList'];  
-  constructor() { }
 
-  ngOnInit() {
-  }
+  constructor() { }
+  ngOnInit() {}
+
+  @Output() headerSelected = new EventEmitter<string>();
+  
+  
+  navigatePath (headerName : string){ 
+      this.headerSelected.emit(headerName);
+    }
 
 }
